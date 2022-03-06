abf35f5f44a61d1dd61f7f98daa753f9c90a7dc3
mohamedarief07 <100037273+mohamedarief07@users.noreply.github.com>
2022-03-06 08:06:10 +0530

    Add files via upload

---
+++ Assignment1org/Desktop.java
@@ -0,0 +1,17 @@
+package Assignment1org.system;
+
+
+
+public class Desktop extends Computer{
+	
+	public void desktopSize() {
+			
+		System.out.println("Size from desktopSize-Desktop");
+		
+	
+
+	}
+		
+
+
+}
---
+++ Assignment1org/Mydesktop.java
@@ -0,0 +1,12 @@
+package Assignment1org.system;
+
+public class Mydesktop {
+	
+	public static void main(String[] args) {
+		
+		Desktop desk=new Desktop();
+		desk.computerModel();
+		desk.desktopSize();
+	}
+
+}
